69487093630c1184950033b16c8f2a8c3842f446
Asa Masterson <asa.masterson@gmail.com>
2023-03-19 22:54:54 +0000

    Add sitemap.xml

--- functions/view.js
+++ functions/view.js
@@ -1,4 +1,8 @@
 export async function onRequestGet({env}) {
   const vws = await env.KV.get("views", "json") ?? 0;
-  return new Response("<h1>Views: " + vws + "</h1>")
+  return new Response("<h1> Views: " + vws + "</h1>", {
+    headers: {
+      'content-type': 'text/html;charset=UTF-8',
+    },
+  });
 }
