5612ecc89af7b248013be9b7f69330af802cd253
Asa Masterson <asa.masterson@gmail.com>
2023-12-05 23:36:01 +0000

    Reorder + add vue Norts

--- functions/index.js
+++
@@ -1,8 +0,0 @@
-export async function onRequestGet({env}) {
-    const vws = await env.KV.get("views", "json") ?? 0;
-    return new Response("<meta name='viewport' content='width=device-width, initial-scale=2.0'><h1> Views: " + vws + "</h1>", {
-        headers: {
-            'content-type': 'text/html;charset=UTF-8',
-        },
-    });
-}
