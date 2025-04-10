d3608c6c694895240733a0e0cc57614f913e2de2
Asa Masterson <asa.masterson@gmail.com>
2025-04-11 00:45:08 +0100

    codify

--- assets/script.js
+++
@@ -1,4 +0,0 @@
-fetch("/api/views")
-.then((response) => response.text())
-.then((response) => {document.getElementById("vc").innerHTML =  "Views - "+response;})
-.catch(function() {document.getElementById("vc").innerHTML = "Err.";});
